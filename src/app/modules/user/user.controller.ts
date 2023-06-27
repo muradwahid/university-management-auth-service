cb1c8a07f705dabdb01ad9f79c0533df3298c9fe
muradwahid <muradwahid334@gmail.com>
2023-06-27 16:13:46 +0600

    dynamic route

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { RequestHandler, Response,Request, NextFunction } from 'express';
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
-// import { z } from 'zod';
 const createUser: RequestHandler = catchAsync(async (req:Request, res:Response,next:NextFunction) => {
     const { user } = req.body
   const result = await UserService.createUser(user)
