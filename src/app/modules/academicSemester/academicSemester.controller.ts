87916d8e91ab15ff91257e5cd17eeaf96aa28086
muradwahid <muradwahid334@gmail.com>
2023-06-27 17:58:09 +0600

    Api Response function completed

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,16 +1,14 @@
 import { NextFunction, Request,Response } from "express";
 import { AcademicSemesterService as AcademicService } from "./academicSemester.service";
 import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
 
 const createSemester = catchAsync(async (req:Request, res:Response,next:NextFunction) => {
   const { ...academicSemesterData } = req.body
   const result = await AcademicService.createSemester(academicSemesterData)
   next()
-  res.status(200).json({
-    success: true,
-    message: 'academic semester is created successfully',
-    data: result,
-  })
+  sendResponse(res,{statusCode:httpStatus.OK,success:true,message:'Academic semester is created successfully',data:result})
 })
 
 export const AcademicSemesterController = {
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,15 +1,19 @@
 import { RequestHandler, Response,Request, NextFunction } from 'express';
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 const createUser: RequestHandler = catchAsync(async (req:Request, res:Response,next:NextFunction) => {
     const { user } = req.body
   const result = await UserService.createUser(user)
   next()
-    res.status(200).json({
-      success: true,
-      message: 'user create successfully',
-      data: result,
-    })
+
+  sendResponse(res,{
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'user create successfully',
+    data:result
+  })
 })
 
 export const UserController= {
