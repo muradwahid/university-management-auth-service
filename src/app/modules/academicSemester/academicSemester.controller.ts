ecb053a4907d1644ef926462e66a7412dd842eed
muradwahid <muradwahid334@gmail.com>
2023-07-02 20:54:19 +0600

    create pick function for pagination

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,7 +3,8 @@ import { AcademicSemesterService, AcademicSemesterService as AcademicService } f
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
-import { IPaginationOptions } from "../../../interfaces/pagination";
+import pick from "../../../shared/pick";
+import { pagination } from "../../../constants/pagination";
 
 const createSemester = catchAsync(async (req:Request, res:Response,next:NextFunction) => {
   const { ...academicSemesterData } = req.body
@@ -14,20 +15,16 @@ const createSemester = catchAsync(async (req:Request, res:Response,next:NextFunc
 })
 
 const getAllSemesters = catchAsync(async (req:Request,res:Response,next:NextFunction) => {
-  const paginationOptions = {
-    page: Number(req.query.page),
-    limit: Number(req.query.limit),
-    sortBy: req.query.sortBy,
-    sortOrder: req.query.sortOrder,
-  }
-  const result = await AcademicSemesterService.getAllSemesters(paginationOptions);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Semester Retrive Successful',
-      data: result,
-    })
-    next()
+  const paginationOptions=pick(req.query,pagination)
+  console.log(paginationOptions);
+  // const result = await AcademicSemesterService.getAllSemesters(paginationOptions);
+  //   sendResponse(res, {
+  //     statusCode: httpStatus.OK,
+  //     success: true,
+  //     message: 'Semester Retrive Successful',
+  //     data: result,
+  //   })
+  //   next()
 })
 
 export const AcademicSemesterController = {
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -5,6 +5,7 @@ import { AcademicSemesterController } from './academicSemester.controller'
 
 const router = express.Router()
 
-router.post('/create-semester',validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),AcademicSemesterController.createSemester)
+router.post('/create-semester', validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema), AcademicSemesterController.createSemester)
+router.get('/',AcademicSemesterController.getAllSemesters)
 
 export  const AcademicSemesterRoutes = router;
